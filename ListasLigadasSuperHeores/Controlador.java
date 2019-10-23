9ad10a9fb6f141d2e799e40db8c276a6676084c9
Chepe Aicrag <48169157+ChepeAicrag12@users.noreply.github.com>
2019-10-23 17:44:01 -0500

    IntentandoArchivo

--- ListasLigadasSuperHeores/Controlador.java
+++ ListasLigadasSuperHeores/Controlador.java
@@ -1,14 +1,12 @@
 public class Controlador
 {
+   /**Prueba de guardar en el archivo*/ 
    public static void main() throws Exception{
        Archivo ar = new Archivo();  
        LinkedListPlusRecursividadGenerico <SuperHeroe >lista = new LinkedListPlusRecursividadGenerico();
        CreacionSuperHeroes cre= new CreacionSuperHeroes();   
-       lista = lista.llenarListaVector(ar.llenarListaDadoNombre("SuperHeroesContenido"));
-       
+       lista = lista.llenarListaVector(cre.arregloSuperHeroes());
        lista.imprimirNormal();
        ar.LlenarVector(lista,"SuperHeroesContenido");
    }
-    
-    
 }
